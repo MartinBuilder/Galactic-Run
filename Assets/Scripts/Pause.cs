b8fd016f481643638d46d9ea27e78e2143d3ae74
Martin Keesom <25179@ma-web.nl>
2018-10-01 14:00:32 +0200

    Space SkyBox
    Nieuwe SkyBox

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Pause : MonoBehaviour
+{/*
+    public Button m_YourFirstButton;
+    public bool isImgOn;
+    public Image Dropdown;
+    public Text Titans;
+    public Text Coins;
+    [SerializeField] private GameObject pausePanel;
+
+    void Start()
+    {
+        Button btn1 = m_YourFirstButton.GetComponent<Button>();
+        
+        btn1.onClick.AddListener(TaskOnClick);
+
+        Dropdown.enabled = false;
+        Titans.enabled = false;
+        Coins.enabled = false;
+        isImgOn = false;
+    }
+
+    void TaskOnClick()
+    {
+            if (!pausePanel.activeInHierarchy)
+            {
+                PauseGame();
+            }
+            if (pausePanel.activeInHierarchy)
+            {
+                ContinueGame();
+            }
+    }
+
+    private void PauseGame()
+    {
+        Time.timeScale = 0;
+        pausePanel.SetActive(true);
+        //Disable scripts that still work while timescale is set to 0
+    }
+    private void ContinueGame()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+        //enable the scripts again
+    }*/
+
+}
