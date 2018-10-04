f3f6ee6650abd74f30fe8a59d33709205782b5b3
Martin Keesom <25179@ma-web.nl>
2018-10-04 10:07:03 +0200

    Movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        //Walk forward
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        //Jump
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("UP");
+        }
+
+        //Left
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Debug.Log("LEFT");
+        }
+
+        //Right
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Debug.Log("RIGHT");
+        }
+
+        transform.Translate(0, 0, z);
+    }
+}
