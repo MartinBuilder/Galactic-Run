a104b1fa0db38116ea27cca5761438765dbd51f3
Martin Keesom <25179@ma-web.nl>
2018-10-05 09:24:00 +0200

    Pauze werkt nu

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -5,49 +5,21 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class Pause : MonoBehaviour
-{/*
-    public Button m_YourFirstButton;
-    public bool isImgOn;
-    public Image Dropdown;
-    public Text Titans;
-    public Text Coins;
-    [SerializeField] private GameObject pausePanel;
+{
+    // Toggles the time scale between 1 and 0.7
+    // whenever the user hits the Fire1 button.
 
-    void Start()
+    void Update()
     {
-        Button btn1 = m_YourFirstButton.GetComponent<Button>();
-        
-        btn1.onClick.AddListener(TaskOnClick);
-
-        Dropdown.enabled = false;
-        Titans.enabled = false;
-        Coins.enabled = false;
-        isImgOn = false;
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (Time.timeScale == 1.0f)
+                Time.timeScale = 0.002f;
+            else
+                Time.timeScale = 1.0f;
+            // Adjust fixed delta time according to timescale
+            // The fixed delta time will now be 0.02 frames per real-time second
+            Time.fixedDeltaTime = 0.02f * Time.timeScale;
+        }
     }
-
-    void TaskOnClick()
-    {
-            if (!pausePanel.activeInHierarchy)
-            {
-                PauseGame();
-            }
-            if (pausePanel.activeInHierarchy)
-            {
-                ContinueGame();
-            }
-    }
-
-    private void PauseGame()
-    {
-        Time.timeScale = 0;
-        pausePanel.SetActive(true);
-        //Disable scripts that still work while timescale is set to 0
-    }
-    private void ContinueGame()
-    {
-        Time.timeScale = 1;
-        pausePanel.SetActive(false);
-        //enable the scripts again
-    }*/
-
 }
