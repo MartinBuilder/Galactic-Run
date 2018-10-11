9c841d5a9255c59b5186206a7952ffe783081562
Martin Keesom <25179@ma-web.nl>
2018-10-11 11:30:57 +0200

    Jump is gefixt
    Jump werkt nu helemaal (Je kan nu alleen springen als je de grond raakt)

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,23 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour {
-    
+
+    private bool isGrounded;
+
 	void Start ()
     {
 		
 	}
-	
-	void Update ()
+
+    void OnTriggerEnter(Collider other)
+    {
+        isGrounded = true;
+    }
+    void OnTriggerExit(Collider other)
+    {
+        isGrounded = false;
+    }
+
+    void Update ()
     {
         //Walk forward
         var z = Time.deltaTime * 10.0f;
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 5.0f;
 
         //Jump
-        if (Input.GetKey(KeyCode.Space))
-        {
-            transform.Translate(x, 1, z);
-        }
+            if (Input.GetKey(KeyCode.Space) && isGrounded == true)
+            {
+                transform.Translate(x, 5, z);
+            }
         transform.Translate(x, 0, z);
     }
 }
